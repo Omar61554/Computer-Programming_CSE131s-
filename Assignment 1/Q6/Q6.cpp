6f28c4e27dee6ce2765a29bb226ff572cb260b2e
Omar Khaled Ahmed <114437079+Omar61554@users.noreply.github.com>
2023-10-29 23:56:24 +0200

    Q6

--- Assignment 1/Q6/Q6.cpp
+++ Assignment 1/Q6/Q6.cpp
@@ -17,7 +17,7 @@ int main() {
     auto currentTime = std::chrono::system_clock::now();
     std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
     struct std::tm currentTime_tm;
-    localtime_s(&currentTime_tm, &currentTime_t);
+    localtime_s(&currentTime_tm, &currentTime_t);  // localtime_s is a secure version of localtime that may pass THE BUFFER to localtime to fill it with data instead of returning a pointer to static data that may be overwritten by subsequent calls to any of the date and time functions.
 
     // Get the due date from the user
     int dueHour, dueMinute;
@@ -40,3 +40,43 @@ int main() {
 
     return 0;
 }
+
+/*  Another solution using ctime library to get the current time and the due date time 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    time_t currentTime = time(nullptr);
+    tm* currentTime_tm = localtime(&currentTime);
+
+    
+    int dueHour, dueMinute;
+    cout << "Enter the due date (hour and minutes): ";
+    cin >> dueHour >> dueMinute;
+
+    
+    currentTime_tm->tm_hour = dueHour;
+    currentTime_tm->tm_min = dueMinute;
+    currentTime_tm->tm_sec = 0;
+
+
+    time_t dueTime =mktime(currentTime_tm);
+
+
+    int minutesDiff = static_cast<int>(difftime(dueTime, currentTime) / 60);
+
+
+    cout << "Number of minutes between current time and due date: " << minutesDiff << "\n"";
+
+    return 0;
+}
+
+    the difference between the two solutions is that the first one uses the chrono library to get the current time and the due date time, 
+    while the second one uses the ctime library to get the current time and the due date time.
+    the first solution is more modern and more accurate than the second one.
+    runtime of the first solution is O(1) while the runtime of the second solution is O(n).
+    */
+
