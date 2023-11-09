9cb336e4469fccdce84af707b927f9f199b54002
Omar Khaled Ahmed <114437079+Omar61554@users.noreply.github.com>
2023-11-09 15:21:23 +0200

    V0.11

--- Assignment 2/Q7/Q7.cpp
+++ Assignment 2/Q7/Q7.cpp
@@ -11,18 +11,31 @@ exceptions before the introduction of the Gregorian calendar on October 15, 1582
 was a leap year). Write a program that asks the user for a year and computes whether that
 year is a leap year. Your program should contain a single if statement.*/
 
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int year;
-    cout << "Enter a year: ";
-    cin >> year;
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-        cout << year << " is a leap year" << endl;
-    else
-        cout << year << " is not a leap year" << endl;
+    while (1)
+    {
+        int year;
+        cout << "Enter a year: ";
+        cin >> year;
+        if (cin.fail())
+        {
+            cout << "Invalid input" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n'); // ignore 1000 characters or until new line
+        }
+        else
+        {
+            if ((year % 4 == 0 && (year < 1582 || year % 100 != 0)) || year % 400 == 0)
+
+                cout << year << " is a leap year" << endl;
+
+            else
+                cout << year << " is not a leap year" << endl;
+        }
+    }
     return 0;
 }
